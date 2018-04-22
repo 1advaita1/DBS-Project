90d3127b96aa750d8c006b6d9261c277e7d9a060
1advaita1 <31865731+1advaita1@users.noreply.github.com>
2018-04-22 22:47:39 +0800

    Extracts all data from files
     Please enter the commit message for your changes. Lines starting

--- songinfo.ps1
+++ songinfo.ps1
@@ -1,5 +1,6 @@
- 
+$path="hbd.mp3"
 cd "C:/Users/advaita/Downloads"
-bash -c "mp3info -p %G Nirvana.mp3" > C:/DBSproj/songinfo/Genree.txt
+bash -c "mp3info -p %a $path" > C:/DBSproj/songinfo/Artists.txt
 bash -c "mp3info -p %y Nirvana.mp3" > C:/DBSproj/songinfo/year.txt
-bash -c "mp3info -p %a Nirvana.mp3" > C:/DBSproj/songinfo/artist.txt
+bash -c "mp3info -p %a hbd.mp3" > C:/DBSproj/songinfo/artist.txt
+pause
