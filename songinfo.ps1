00333670de9bd02a6a52e4eaaab1f566c8f93152
1advaita1 <31865731+1advaita1@users.noreply.github.com>
2018-04-24 18:36:58 +0800

    corrected songinfo.ps1; album_year_concat remains

--- songinfo.ps1
+++ songinfo.ps1
@@ -1,17 +1,27 @@
 gcb > "C:/DBSproj/songinfo/filepath.txt"
-
+echo  obtained path
+pause
 $filepat = Get-Content "C:\DBSproj\songinfo\filepath.txt"
 $pathh=Get-ChildItem "$filepat" 
 $pathh.Name > C:\DBSproj\songinfo\Filename.txt
 $filename=Get-Content "C:/DBSproj/songinfo/Filename.txt"
+echo  obtained Name
+pause
+bash -c "pwd"
 
-bash -c "mp3info -p %F $filename" > C:/DBSproj/songinfo/Artists.txt
 bash -c "mp3info -p %a $filename" > C:/DBSproj/songinfo/Artists.txt
+Get-Content C:/DBSproj/songinfo/Artists.txt | Set-Content -Encoding utf8 Artists.txt
 bash -c "mp3info -p %y $filename" > C:/DBSproj/songinfo/Year.txt
+Get-Content C:/DBSproj/songinfo/Year.txt | Set-Content -Encoding utf8 Year.txt
 bash -c "mp3info -p %l $filename" > C:/DBSproj/songinfo/Album.txt
+Get-Content C:/DBSproj/songinfo/Album.txt | Set-Content -Encoding utf8 Album.txt
 bash -c "mp3info -p %g $filename" C:/DBSproj/songinfo/Genre.txt
+Get-Content C:/DBSproj/songinfo/Genre.txt | Set-Content -Encoding utf8 Genre.txt
 
-start Album_year_concat.bat
-
+echo  obtained metadata
+pause
+start C:/DBSproj/songinfo/Album_year_concat.bat
+Get-Content C:/DBSproj/songinfo/Album_year_concat.txt | Set-Content -Encoding utf8 Album_year_concat.txt
+pause
 
 
